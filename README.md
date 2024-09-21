f529e7ad66141e22d47f5cb8ad1df465f2288ae3
Davon Morris <rootbeer587@gmail.com>
2024-09-21 18:59:48 -0500

    ReadMe, gitignore, requirements, and some beginning touches

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# pandas
+### INF601 - Advanced Programming in Python
+### Davon Morris
+### Mini Project 2
+
+
+# Mini Project 2 - Pandas
+
+## Description
+
+The goal is to come up with a question and then see what API I can use to get the information needed to answer that question.
+
+## Getting Started
+
+### Dependencies
+
+```
+pip install -r requirements.txt
+```
+### Installing
+
+* Clone my repository to your IDE configured to run Python 3.12
+* Optional - Create directory named "charts", if not created the script will do it for you
+
+### Executing program
+
+* Create an interpreter for this project
+* pip install -r requirements.txt
+* Choose your tickers by placing them in the "myTickers" list on line 10 or use the pre-populated ones
+* Press play and open the images created in the "charts" directory
+
+## Authors
+
+Davon Morris
+
+## Acknowledgments
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+# df = pd.DataFrame(
+#     {
+#         "Name": [
+#             "Braund, Mr. Owen Harris",
+#             "Allen, Mr. William Henry",
+#             "Bonnell, Miss. Elizabeth",
+#         ],
+#         "Age": [22, 35, 58],
+#         "Sex": ["male", "male", "female"],
+#     }
+# )
+#
+# level = pd.Series(['sophmore', 'freshman', 'senior'], name = 'Student Level')
+#
+#
+# print(level)
+# #print(list(df.columns))
+# #print(df['Age'])
 import numpy as np
 import pandas as pd
+import requests
 
-df = pd.DataFrame(
-    {
-        "Name": [
-            "Braund, Mr. Owen Harris",
-            "Allen, Mr. William Henry",
-            "Bonnell, Miss. Elizabeth",
-        ],
-        "Age": [22, 35, 58],
-        "Sex": ["male", "male", "female"],
-    }
-)
-
-level = pd.Series(['sophmore', 'freshman', 'senior'], name = 'Student Level')
-
-
-print(level)
-#print(list(df.columns))
-#print(df['Age'])
+name = 'golden retriever'
+api_url = 'https://api.api-ninjas.com/v1/dogs?name={}'.format(name)
+response = requests.get(api_url, headers={'X-Api-Key': 'YOUR_API_KEY'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
